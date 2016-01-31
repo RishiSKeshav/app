9806d35e88f8cf1d3dbf4c491325d48582937b89
amit70 <amit.rajula@gmail.com>
2016-01-31 12:09:19 -0800

    Register Activity

--- app/src/main/java/com/rishi/app/app/LoginActivity.java
+++ app/src/main/java/com/rishi/app/app/LoginActivity.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -28,7 +29,7 @@ import java.io.UnsupportedEncodingException;
 
 
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends ActionBarActivity {
 
     SessionManager sessionManager;
 
