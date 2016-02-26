375684c3d5e6a5d28e9e0e84bbb22820815500ff
RishiSKeshav <rishiskeshav91@gmail.com>
2016-02-25 21:18:23 -0800

    network change identification

--- app/src/main/java/com/rishi/app/app/ImageUploadService.java
+++ app/src/main/java/com/rishi/app/app/ImageUploadService.java
@@ -99,116 +99,122 @@ public class ImageUploadService extends Service {
     }
 
     public void upload(Image img) {
-        File imgFile = new File(img.getPath());
 
-        if (imgFile.exists()) {
-            Intent intent = new Intent("IMAGE_ACTION");
-            intent.putExtra("imgPath", img.path);
-            intent.putExtra("leftCount", count);
-            sendBroadcast(intent);
+        if(sessionManager.getSyncStatus()) {
 
-            Log.d("userID", sessionManager.getId());
-            String filePath = img.getPath();
-            String filename = img.getName();
+            File imgFile = new File(img.getPath());
 
-            Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+            if (imgFile.exists()) {
+                Intent intent = new Intent("IMAGE_ACTION");
+                intent.putExtra("imgPath", img.path);
+                intent.putExtra("leftCount", count);
+                sendBroadcast(intent);
 
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.PNG, 50, stream);
-            byte[] byte_arr = stream.toByteArray();
-            String encodedString = Base64.encodeToString(byte_arr, 0);
+                Log.d("userID", sessionManager.getId());
+                String filePath = img.getPath();
+                String filename = img.getName();
 
-            String responseString = null;
+                Bitmap bitmap = BitmapFactory.decodeFile(filePath);
 
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost("http://52.89.2.186/project/webservice/uploadMedia.php");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 50, stream);
+                byte[] byte_arr = stream.toByteArray();
+                String encodedString = Base64.encodeToString(byte_arr, 0);
 
-            try {
-                AndroidMultiPartEntity entity = new AndroidMultiPartEntity(
-                        new AndroidMultiPartEntity.ProgressListener() {
+                String responseString = null;
 
-                            @Override
-                            public void transferred(long num) {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost("http://52.89.2.186/project/webservice/uploadMedia.php");
 
-                                double y = (long) (((float) num / totalSize) * 100);
+                try {
+                    AndroidMultiPartEntity entity = new AndroidMultiPartEntity(
+                            new AndroidMultiPartEntity.ProgressListener() {
 
-                                Intent i = new Intent("PROGRESS_ACTION");
-                                i.putExtra("NUMBER", (int) y);
-                                sendBroadcast(i);
+                                @Override
+                                public void transferred(long num) {
+
+                                    double y = (long) (((float) num / totalSize) * 100);
+
+                                    Intent i = new Intent("PROGRESS_ACTION");
+                                    i.putExtra("NUMBER", (int) y);
+                                    sendBroadcast(i);
+                                }
+                            });
+
+                    // Adding file data to http body
+                    entity.addPart("image", new StringBody(encodedString));
+
+                    // Extra parameters if you want to pass to server
+                    entity.addPart("userId", new StringBody(sessionManager.getId()));
+                    entity.addPart("filename", new StringBody(filename));
+                    File f = new File(filename);
+                    entity.addPart("filesize", new StringBody(String.valueOf(f.length())));
+
+                    totalSize = entity.getContentLength();
+                    httppost.setEntity(entity);
+
+                    Log.d("entity",entity.toString());
+
+                    // Making server call
+                    HttpResponse response = httpclient.execute(httppost);
+                    HttpEntity r_entity = response.getEntity();
+
+                    int statusCode = response.getStatusLine().getStatusCode();
+                    if (statusCode == 200) {
+                        // Server response
+                        responseString = EntityUtils.toString(r_entity);
+
+                        Log.d("response", responseString);
+                        try {
+                            JSONObject obj = new JSONObject(responseString);
+
+                            if (obj.getBoolean("error")) {
+
+                                Log.d("file error", obj.getString("msg"));
+                                //Toast.makeText(getApplicationContext(), obj.getString("msg"), Toast.LENGTH_LONG).show();
+                            } else {
+
+                                int mediaId = Integer.parseInt(obj.getString("mediaId"));
+                                String link = obj.getString("link");
+
+                                Log.d("response ok", String.valueOf(mediaId) + " " + link);
+
+                                Log.d("link", link);
+
+                                File folder = new File(Environment.getExternalStorageDirectory().toString() + "/app");
+                                boolean success = true;
+                                if (!folder.exists()) {
+                                    success = folder.mkdir();
+                                }
+
+                                db = new ImageDatabaseHandler(getApplicationContext(), folder.toString());
+
+                                Image image = new Image();
+
+                                image.setId(mediaId);
+                                image.setPath(filePath);
+                                image.setName(filename);
+                                image.setLink(link);
+
+                                db.addImage(image);
+
+                                count--;
+                                Log.d("count", String.valueOf(count));
                             }
-                        });
-
-                // Adding file data to http body
-                entity.addPart("image", new StringBody(encodedString));
-
-                // Extra parameters if you want to pass to server
-                entity.addPart("userId", new StringBody(sessionManager.getId()));
-                entity.addPart("filename", new StringBody(filename));
-                File f = new File(filename);
-                entity.addPart("filesize", new StringBody(String.valueOf(f.length())));
-
-                totalSize = entity.getContentLength();
-                httppost.setEntity(entity);
-
-                // Making server call
-                HttpResponse response = httpclient.execute(httppost);
-                HttpEntity r_entity = response.getEntity();
-
-                int statusCode = response.getStatusLine().getStatusCode();
-                if (statusCode == 200) {
-                    // Server response
-                    responseString = EntityUtils.toString(r_entity);
-
-                    Log.d("response",responseString);
-                    try {
-                        JSONObject obj = new JSONObject(responseString);
-
-                        if (obj.getBoolean("error")) {
-
-                            Log.d("file error",obj.getString("msg"));
-                            //Toast.makeText(getApplicationContext(), obj.getString("msg"), Toast.LENGTH_LONG).show();
-                        } else {
-
-                            int mediaId = Integer.parseInt(obj.getString("mediaId"));
-                            String link = obj.getString("link");
-
-                            Log.d("response ok",String.valueOf(mediaId) + " " + link);
-
-                            Log.d("link", link);
-
-                            File folder = new File(Environment.getExternalStorageDirectory().toString() + "/app");
-                            boolean success = true;
-                            if (!folder.exists()) {
-                                success = folder.mkdir();
-                            }
-
-                            db = new ImageDatabaseHandler(getApplicationContext(), folder.toString());
-
-                            Image image = new Image();
-
-                            image.setId(mediaId);
-                            image.setPath(filePath);
-                            image.setName(filename);
-                            image.setLink(link);
-
-                            db.addImage(image);
-
-                            count--;
-                            Log.d("count",String.valueOf(count));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+
+                    } else {
+                        responseString = "Error occurred! Http Status Code: "
+                                + statusCode;
                     }
 
-                } else {
-                    responseString = "Error occurred! Http Status Code: "
-                            + statusCode;
+                } catch (ClientProtocolException e) {
+                    responseString = e.toString();
+                } catch (IOException e) {
+                    responseString = e.toString();
                 }
-
-            } catch (ClientProtocolException e) {
-                responseString = e.toString();
-            } catch (IOException e) {
-                responseString = e.toString();
             }
         }
     }
---
+++ app/src/main/java/com/rishi/app/app/NetworkChangeReceiver.java
@@ -0,0 +1,162 @@
+package com.rishi.app.app;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by RishiS on 2/25/2016.
+ */
+public class NetworkChangeReceiver extends BroadcastReceiver {
+    SessionManager sessionManager;
+    Intent serviceIntent;
+
+    private ArrayList<com.rishi.app.app.Image> imageList;
+    private ArrayList<com.rishi.app.app.Image> syncedImageList;
+    private ArrayList<com.rishi.app.app.Image> unSyncedImageList;
+    private ArrayList<String> syncedPathList;
+
+    Context c;
+
+    public NetworkChangeReceiver(){
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        sessionManager = new SessionManager(context.getApplicationContext());
+        c = context.getApplicationContext();
+        Log.d("network change", "network change");
+        Log.d("onReceive",sessionManager.getName());
+
+        startOrStopSync();
+
+    }
+
+    public void startOrStopSync() {
+    ConnectivityManager cm =
+            (ConnectivityManager) c.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+    if (activeNetwork != null && activeNetwork.isConnected()) {
+
+        boolean isMobileData = activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE;
+        boolean isWIFI = activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
+
+        if (sessionManager.getSyncStatus()) {
+                        if (isWIFI) {
+                startSync();
+            } else if (isMobileData) {
+                if (sessionManager.getPhotoSyncStatus())
+                    startSync();
+                else
+                    stopSync();
+            }
+
+            initializeImageLists();
+
+            serviceIntent = new Intent(c,ImageUploadService.class);
+            serviceIntent.putParcelableArrayListExtra("unSyncedImageList", unSyncedImageList);
+            c.startService(serviceIntent);
+        } else {
+
+            stopSync();
+        }
+    }
+}
+
+    private void startSync(){
+
+        initializeImageLists();
+
+        serviceIntent = new Intent(c,ImageUploadService.class);
+        serviceIntent.putParcelableArrayListExtra("unSyncedImageList", unSyncedImageList);
+        c.startService(serviceIntent);
+    }
+
+    private void stopSync(){
+        serviceIntent = new Intent(c, ImageUploadService.class);
+        c.stopService(serviceIntent);
+    }
+
+
+    private void initializeImageLists() {
+
+        generateImageList();
+        generateDBImageList();
+        generateUnsyncedImageList();
+    }
+
+    private void generateUnsyncedImageList() {
+
+        unSyncedImageList = new ArrayList<Image>();
+
+        for(com.rishi.app.app.Image img : imageList){
+
+            if(!syncedPathList.contains(img.path))
+                unSyncedImageList.add(img);
+        }
+
+        Log.d("unsynced count", String.valueOf(unSyncedImageList.size()));
+    }
+
+    private void generateDBImageList() {
+
+        ImageDatabaseHandler db = new ImageDatabaseHandler(c, Environment.getExternalStorageDirectory().toString()+ "/app");
+
+        if(db!=null){
+
+            syncedPathList= db.getAllImagePath();
+
+            Iterator<String> abc = syncedPathList.iterator();
+
+            while(abc.hasNext())
+            {
+
+                Log.d("DB ",abc.next());
+            }
+
+        }
+        else
+            Log.i("databasae_name","DB not created");
+
+        Log.d("DB list count ", String.valueOf(syncedPathList.size()));
+    }
+
+    private void generateImageList() {
+
+        imageList = new ArrayList<com.rishi.app.app.Image>();
+
+        ContentResolver cr = c.getContentResolver();
+
+        String[] columns = new String[] {
+                MediaStore.Images.ImageColumns.DATA,
+                MediaStore.Images.ImageColumns.DISPLAY_NAME};
+        Cursor cursor = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                columns, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                com.rishi.app.app.Image image = new com.rishi.app.app.Image();
+
+                image.setPath(cursor.getString(0));
+                image.setName(cursor.getString(1));
+
+                imageList.add(image);
+            } while (cursor.moveToNext());
+        }
+
+        Log.d("image list count", String.valueOf(imageList.size()));
+    }
+}
