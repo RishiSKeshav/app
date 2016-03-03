67dbc3a0762647c2aef0ebc70bebeb67514071cd
amit70 <amit.rajula@gmail.com>
2016-03-02 19:29:00 -0800

    new update

--- app/src/main/java/com/rishi/app/app/EmailAuthentication.java
+++ app/src/main/java/com/rishi/app/app/EmailAuthentication.java
@@ -2,13 +2,26 @@ package com.rishi.app.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.nispok.snackbar.SnackbarManager;
+
+import org.apache.http.entity.StringEntity;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by amitrajula on 2/20/16.
@@ -78,8 +91,173 @@ public class EmailAuthentication extends AppCompatActivity {
             }
         });
 
+        TextView noCode = (TextView)findViewById(R.id.noCode);
+        noCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(ACTION.equals("")){
+
+                    try {
+                        JSONObject obj = new JSONObject();
+                        obj.put("emailId", EMAILID);
+
+                        StringEntity jsonString = new StringEntity(obj.toString());
 
 
+                        AsyncHttpClient client = new AsyncHttpClient();
+
+                        client.post(getApplicationContext(), "http://52.89.2.186/project/webservice/public/Register", jsonString, "application/json", new AsyncHttpResponseHandler() {
+
+                            @Override
+                            public void onStart() {
+                                // called before request is started
+                            }
+
+                            // @Override
+                            public void onSuccess(String response) {
+                                // called when response HTTP status is "200 OK"
+                                try {
+                                    JSONObject obj = new JSONObject(response);
+
+                                    if (obj.getBoolean("error")) {
+                                        SnackbarManager.show(
+                                                com.nispok.snackbar.Snackbar.with(EmailAuthentication.this)
+                                                        .text("Something went wrong")
+                                                        .duration(com.nispok.snackbar.Snackbar.SnackbarDuration.LENGTH_SHORT)
+                                        );
+                                    } else {
+
+                                        if(obj.getBoolean("user")){
+                                            SnackbarManager.show(
+                                                    com.nispok.snackbar.Snackbar.with(EmailAuthentication.this)
+                                                            .text("User with emailId already exist")
+                                                            .duration(com.nispok.snackbar.Snackbar.SnackbarDuration.LENGTH_SHORT)
+                                            );
+
+                                        }else{
+
+//                                            Intent i = new Intent(EmailAuthentication.this,EmailAuthentication.class);
+//                                            i.putExtra("code",obj.getString("code"));
+//                                            i.putExtra("emailId",object.optString("emailId"));
+//                                            startActivity(i);
+//                                            finish();
+
+                                            SnackbarManager.show(
+                                                    com.nispok.snackbar.Snackbar.with(EmailAuthentication.this)
+                                                            .text("Code sent to " + EMAILID)
+                                                            .duration(com.nispok.snackbar.Snackbar.SnackbarDuration.LENGTH_SHORT)
+                                            );
+
+                                        }
+
+                                    }
+
+                                } catch (JSONException e) {
+                                    // TODO Auto-generated catch block
+                                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                                    e.printStackTrace();
+
+                                }
+                            }
+
+                            //@Override
+                            public void onFailure(int statusCode, PreferenceActivity.Header[] headers, byte[] errorResponse, Throwable e) {
+                                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                            }
+
+                            //@Override
+                            public void onRetry(int retryNo) {
+                                // called when request is retried
+                            }
+
+
+                        });
+
+                    }catch (JSONException e) {
+                        e.printStackTrace();
+                    }catch(UnsupportedEncodingException ee){
+                        ee.printStackTrace();
+                    }
+
+                }else{
+
+
+                    try {
+                        JSONObject obj = new JSONObject();
+                        obj.put("emailId", EMAILID);
+
+                        StringEntity jsonString = new StringEntity(obj.toString());
+
+
+                        AsyncHttpClient client = new AsyncHttpClient();
+
+                        client.post(getApplicationContext(), "http://52.89.2.186/project/webservice/public/Getcode", jsonString, "application/json", new AsyncHttpResponseHandler() {
+
+                            @Override
+                            public void onStart() {
+                                // called before request is started
+                            }
+
+                            // @Override
+                            public void onSuccess(String response) {
+                                // called when response HTTP status is "200 OK"
+                                try {
+                                    JSONObject obj = new JSONObject(response);
+
+                                    if (obj.getBoolean("error")) {
+                                        SnackbarManager.show(
+                                                com.nispok.snackbar.Snackbar.with(EmailAuthentication.this)
+                                                        .text("Something went wrong")
+                                                        .duration(com.nispok.snackbar.Snackbar.SnackbarDuration.LENGTH_SHORT)
+                                        );
+                                    } else {
+//                                        Intent i = new Intent(GetCode.this,EmailAuthentication.class);
+//                                        i.putExtra("code",obj.getString("code"));
+//                                        i.putExtra("emailId",EMAILID);
+//                                        i.putExtra("action","password_rest");
+//                                        startActivity(i);
+//                                        finish();
+
+                                        SnackbarManager.show(
+                                                com.nispok.snackbar.Snackbar.with(EmailAuthentication.this)
+                                                        .text("Code has been sent to " + EMAILID)
+                                                        .duration(com.nispok.snackbar.Snackbar.SnackbarDuration.LENGTH_SHORT)
+                                        );
+
+
+                                    }
+
+                                } catch (JSONException e) {
+                                    // TODO Auto-generated catch block
+                                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                                    e.printStackTrace();
+
+                                }
+                            }
+
+                            //@Override
+                            public void onFailure(int statusCode, PreferenceActivity.Header[] headers, byte[] errorResponse, Throwable e) {
+                                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                            }
+
+                            //@Override
+                            public void onRetry(int retryNo) {
+                                // called when request is retried
+                            }
+
+
+                        });
+
+                    }catch (JSONException e) {
+                        e.printStackTrace();
+                    }catch(UnsupportedEncodingException ee){
+                        ee.printStackTrace();
+                    }
+
+                }
+            }
+        });
 
     }
 }
